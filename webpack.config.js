9f5b2b5cb1c2fc06df4f1c09da3e984a03cc5feb
aagraston <aagraston@gmail.com>
2022-02-14 11:46:36 -0800

    eslint and prettier added, vscode settings updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-const path = require('path');
-const { mainModule } = require('process');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
